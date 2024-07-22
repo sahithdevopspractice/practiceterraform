ddc23fc4776cd20208a025abbd3d19f1df1e2252
sahithdevopspractice <sahithdevopspractice@gmail.com>
2024-07-22 15:25:04 -0400

    security group

--- ec2.tf
+++ ec2.tf
@@ -7,3 +7,29 @@ resource "aws_instance" "web" {
   }
 }
 
+resource "aws_security_group" "robhoshop" {
+  name        = "robhoshop-aws"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  #vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "robhoshop-aws"
+  }
+
+  ingress {
+    description = "Allow All Ports"
+    from_port = 0
+    to_port = 0
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
