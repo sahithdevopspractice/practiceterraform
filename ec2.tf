ff7570aa898ccb7ae64d63a93e4aa1c4f4584d0c
sahithdevopspractice <sahithdevopspractice@gmail.com>
2024-07-22 16:06:50 -0400

    security group

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.robhoshop.id]
 
   tags = {
     Name = "ec2_instance"
